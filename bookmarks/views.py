9cb51771a8d9828d068b0ea89cdcef561b1f42ac
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2018-06-28 15:37:16 -0500

    Bookmarks page running along with Personal Bookmarks page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 # Create your views here.
 
@@ -7,5 +7,11 @@ def index(request):
     context = {
         'bookmarks': Bookmark.objects.all(),
     }
-    
+    if request.user.is_anonymous:
+        context['personal_bookmarks'] = PersonalBookmark.objects.none()
+    else:
+        context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+
+        
+
     return render(request, 'bookmarks/index.html', context)
