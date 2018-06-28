ffdc97db955c9e595e2b6212498a050bac2e588e
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2018-06-28 15:42:01 -0500

    Notes page and Personal Notes page has been cretaed

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Note
+from .models import Note, PersonalNote
 
 # Create your views here.
 
@@ -7,5 +7,11 @@ def index(request):
     context = {
         'notes': Note.objects.all(),
     }
-    
+    if request.user.is_anonymous:
+        context['personal_notes'] = PersonalNote.objects.none()
+    else:
+        context['personal_notes'] = PersonalNote.objects.filter(user=request.user)
+
+        
+
     return render(request, 'notes/index.html', context)
