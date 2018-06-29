2ddb16ae8a112b29951d970f8dbe174927a9072c
RedHoodJT1988 <31198731+RedHoodJT1988@users.noreply.github.com>
2018-06-29 12:04:49 -0500

    Installed Heroku

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,8 +4,11 @@ from .models import Bookmark, PersonalBookmark
 # Create your views here.
 
 def index(request):
+    
+    pbid = PersonalBookmark.objects.values_list('id')
+
     context = {
-        'bookmarks': Bookmark.objects.all(),
+        'bookmarks': Bookmark.objects.exclude(id__in=pbid)
     }
     if request.user.is_anonymous:
         context['personal_bookmarks'] = PersonalBookmark.objects.none()
